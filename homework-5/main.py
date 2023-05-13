c96dcbe9865155c043d2c412db72a9b4e224c23d
Лилия Галиуллина <lili_gal@mail.ru>
2023-05-13 16:14:03 +0500

    add playlist.py

--- homework-5/main.py
+++ homework-5/main.py
@@ -1,5 +1,4 @@
 import datetime
-
 from src.playlist import PlayList
 
 if __name__ == '__main__':
@@ -8,8 +7,8 @@ if __name__ == '__main__':
     assert pl.url == "https://www.youtube.com/playlist?list=PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb"
 
     duration = pl.total_duration
+    print (str(duration))
     assert str(duration) == "3:41:01"
     assert isinstance(duration, datetime.timedelta)
     assert duration.total_seconds() == 13261.0
-
     assert pl.show_best_video() == "https://youtu.be/9Bv2zltQKQA"
