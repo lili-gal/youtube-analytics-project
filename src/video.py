c3164621cf522085e5170aceafb9758e23fa3aba
Лилия Галиуллина <lili_gal@mail.ru>
2023-06-15 13:55:48 +0500

    change class Video

--- src/video.py
+++ src/video.py
@@ -1,5 +1,6 @@
 import os
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -10,13 +11,22 @@ class Video:
         self.video_id = video_id
         YT_API_KEY = os.getenv('YT_API_KEY')
         youtube = build('youtube', 'v3', developerKey=YT_API_KEY)
-        video = youtube.videos().list(part='snippet,statistics', id=self.video_id).execute()['items'][0]['snippet']
-        self.title = video['title']
-        self.url = f"https://www.youtube.com/watch?v={self.video_id}"
-        self.views = int(
-            youtube.videos().list(part='statistics', id=self.video_id).execute()['items'][0]['statistics']['viewCount'])
-        self.likes = int(
-            youtube.videos().list(part='statistics', id=self.video_id).execute()['items'][0]['statistics']['likeCount'])
+        try:
+            video = youtube.videos().list(part='snippet,statistics', id=self.video_id).execute()
+            self.title = video[0]['snippet']['title']
+            self.url = f"https://www.youtube.com/watch?v={self.video_id}"
+            self.views = int(
+                youtube.videos().list(part='statistics', id=self.video_id).execute()['items'][0]['statistics'][
+                    'viewCount'])
+            self.like_count = int(
+                youtube.videos().list(part='statistics', id=self.video_id).execute()['items'][0]['statistics'][
+                    'likeCount'])
+        except KeyError as e:
+            print('Видео не найдено')
+            self.title = None
+            self.like_count = None
+            self.url = None
+            self.views = None
 
     def __str__(self):
         return self.title
@@ -36,6 +46,7 @@ class PLVideo(Video):
         self.likes = int(
             youtube.videos().list(part='statistics', id=self.video_id).execute()['items'][0]['statistics']['likeCount'])
         self.playlist_id = playlist_id
+        # like_count: int = youtube.videos().list['items'][0]['statistics']['likeCount']
 
     def __str__(self):
         return self.title
